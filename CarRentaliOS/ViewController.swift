d26e127c8cd050c6b4799138021aa78a51b0753e
Ing. Ebu Celik <ebucelik1@hotmail.com>
2023-04-03 19:36:36 +0200

    Move from SwiftUI to UIKit

--- CarRentaliOS/ViewController.swift
+++ CarRentaliOS/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import SwiftUI
+import ComposableArchitecture
 
 class ViewController: UIViewController {
 
+    let appView = UIHostingController(
+        rootView: AppView(
+            store: Store(
+                initialState: AppCore.State(
+                    homeState: HomeCore.State(),
+                    entryState: EntryCore.State(
+                        loginState: LoginCore.State(
+                            customer: .emptyCustomer
+                        )
+                    )
+                ),
+                reducer: AppCore()
+            )
+        )
+    )
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .clear
+
+        setupSubViews()
+        setupConstraints()
     }
 
+    func setupSubViews() {
+        appView.view.translatesAutoresizingMaskIntoConstraints = false
 
+        view.addSubview(appView.view)
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate(
+            [
+                appView.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                appView.view.topAnchor.constraint(equalTo: view.topAnchor),
+                appView.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                appView.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ]
+        )
+    }
 }
 
