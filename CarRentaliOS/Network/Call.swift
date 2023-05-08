3154d7ee2c965fb7ecd0fddbe3a1e6dbc72af383
Ing. Ebu Celik <ebucelik1@hotmail.com>
2023-05-08 13:57:13 +0200

    change call

--- CarRentaliOS/Network/Call.swift
+++ CarRentaliOS/Network/Call.swift
@@ -19,7 +19,8 @@ public protocol Call where Self.Response: Codable {
 }
 
 extension Call {
-    var httpScheme: String { "http://ec2-3-72-108-183.eu-central-1.compute.amazonaws.com:8080/api/v1/" }
+    var part1: String { "http://ec2-3-72-108-183.eu-central-1.compute.amazonaws.com:8080/api/v1/" }
+    var httpScheme: String { part1 }
     var parameters: [String : Any]? { nil }
     var body: Codable? { nil }
 }
